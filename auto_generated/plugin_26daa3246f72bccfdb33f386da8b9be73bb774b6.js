7ec51f3cf2d6eeef823202e5b8826c9e2b07d28b
terrapop <T7Edo4tco41H>
2020-09-15 00:08:21 +0200

    Delete autogenerated.

--- auto_generated/plugin_26daa3246f72bccfdb33f386da8b9be73bb774b6.js
+++
@@ -1,6 +0,0 @@
-(function(){$(document).on('click', '.ytlight', function() {
-    var iframe = $('<iframe src="'+$(this).data("embed-url")+'" scrolling="no" frameborder="0" style="border: none;"></iframe>');
-    // Replace the poster image with player
-    $(this).empty().append(iframe);
-});
-})();
